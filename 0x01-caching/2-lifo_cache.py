4c2e06291f46158c7756e890ee0df87103c4b629
BarakaCaleb <calebbaraka79@gmail.com>
2024-08-21 20:40:25 +0300

    Task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,40 +1,65 @@
 #!/usr/bin/env python3
-# Importing the BaseCaching class from the base_caching module
-from base_caching import BaseCaching
+"""Create a class LIFOCache that inherits from
+BaseCaching and is a caching system:
+
+You must use self.cache_data - dictionary from
+the parent class BaseCaching
+You can overload def __init__(self): but don’t
+forget to call the parent init: super().__init__()
+def put(self, key, item):
+Must assign to the dictionary self.cache_data the
+item value for the key key.
+If key or item is None, this method should not do
+anything.
+If the number of items in self.cache_data is higher
+that BaseCaching.MAX_ITEMS:
+you must discard the last item put in cache
+(LIFO algorithm)
+you must print DISCARD: with the key discarded and
+following by a new line
+def get(self, key):
+Must return the value in self.cache_data linked to key.
+If key is None or if the key doesn’t exist in
+self.cache_data, return None.
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
 
 class LIFOCache(BaseCaching):
-    """ LIFOCache class that inherits from BaseCaching
-        Implements a caching system with LIFO (Last-In-First-Out) strategy
+    """_summary_
     """
 
     def __init__(self):
-        """ Initialize the class """
-        super().__init__()  # Call the parent class's init method
-        self.stack = []  # To keep track of the insertion order
+        """_summary_
+        """
+        super().__init__()
 
     def put(self, key, item):
-        """ Add an item in the cache
-            If key or item is None, do nothing
+        """_summary_
+
+        Args:
+                        key (_type_): _description_
+                        item (_type_): _description_
         """
         if key is None or item is None:
-            return
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                # delete the last item in the dictionary
+                last_key, last_value = self.cache_data.popitem()
+                print("DISCARD: {}". format(last_key))
 
-        # If key already exists, remove it from the stack to update its position
-        if key in self.cache_data:
-            self.stack.remove(key)
-        
-        # Add the new key to the stack and the cache
-        self.cache_data[key] = item
-        self.stack.append(key)
-
-        # If the cache exceeds MAX_ITEMS, remove the last item added (LIFO)
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            last_key = self.stack.pop(-2)  # The last one before the current
-            del self.cache_data[last_key]
-            print(f"DISCARD: {last_key}")
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key
-            If key is None or doesn't exist, return None
+        """return the value in self.cache_data linked to key
+
+        Args:
+                        key (_type_): _description_
         """
-        return self.cache_data.get(key, None)
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
